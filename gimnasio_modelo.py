5fe13a38bdeff16369cade3612e176b3706505b9
viviana <vivianalorenaavellaneda@gmail.com>
2025-11-03 17:25:28 -0300

    creando clases

---
+++ gimnasio_modelo.py
@@ -0,0 +1,41 @@
+class Socio: 
+    def __init__(self, id_socio, nombre_apellido, direccion, fecha_nacimiento, telefono, email, fecha_registro,
+                talle, peso, objetivo):
+        self.id_socio = id_socio
+        self.nombre_apellido = nombre_apellido
+        self.direccion = direccion
+        self.fecha_nacimiento = 
+        self.telefono = telefono
+        self.email= email
+        self.fecha_registro= 
+        self.talle = talle
+        self.peso = peso
+        self.objetivo = objetivo
+
+
+
+
+
+
+class rutina:
+    ...
+
+
+
+class equipamiento:
+    ...
+
+
+
+
+class clases:
+    ...
+
+
+
+class horarios:
+    ...
+
+
+class instructor:
+    ...    
